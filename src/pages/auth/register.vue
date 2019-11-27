8a872328f138aef5e2bfc8f3495e1fdece9d7ca2
webbiqing <webwbqing@163.com>
2019-11-27 18:09:13 +0800

    注册校验

--- src/pages/auth/register.vue
+++ src/pages/auth/register.vue
@@ -5,18 +5,22 @@
     </div>
     <van-cell-group>
       <van-field
+        left-icon="contact"
         v-model="username"
         required
         clearable
         label="用户名"
         placeholder="请输入用户名"
+        :error-message="errorMsg.username"
       />
 
       <van-field
+        left-icon="question"
         v-model="password"
         type="password"
         label="密码"
         placeholder="请输入密码"
+        :error-message="errorMsg.pwd"
         required
       />
     </van-cell-group>
@@ -36,10 +40,35 @@
       return {
         username: '',
         password: '',
+        errorMsg:{
+          username:'',
+          pwd:''
+        }
       }
     },
     methods: {
+      verifyUserName(){
+        var reg1 = new RegExp(/^[0-9A-Za-z]+$/);
+        if(!this.username){
+          this.errorMsg.username = '用户名不能为空'
+          return false
+        }else if (!reg1.test(this.username)) {
+          this.errorMsg.username = '用户名只能为字母跟数字组合'
+          return false;
+        }else if(this.username.length > 15){
+          this.errorMsg.username = '用户名不能超过15位'
+          return false;
+        }
+        return true;
+      },
       async register() {
+        this.errorMsg = {};
+        let userFlag = this.verifyUserName();
+        if(!userFlag) return ;
+        if(this.password.length > 15){
+          this.errorMsg.pwd = '密码不能超过15位'
+          return ;
+        }
         const { data,code,message } = await register({username: this.username, pwd: this.password});
         if (code === 200) {
           Toast.success('注册成功，前往登录');
